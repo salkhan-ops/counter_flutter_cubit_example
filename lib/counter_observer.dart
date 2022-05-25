b36e32f9928d2a7898562a6660c414cd778f9241
salkhan-ops <65277891+salkhan-ops@users.noreply.github.com>
2022-05-25 07:39:02 +0500

    my added files

--- lib/counter_observer.dart
+++ lib/counter_observer.dart
@@ -7,6 +7,6 @@ class CounterObserver extends BlocObserver{
   @override
   void onChange(BlocBase bloc, Change change) {
     super.onChange(bloc, change);
-    print('${bloc.runtimeType} $change);
+    print('${bloc.runtimeType} $change');
   }
 }
